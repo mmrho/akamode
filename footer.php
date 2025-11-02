a62a7cbbf59d5dd9b47a2c49b072eb136e7a633d
mmrho <mmohammadrezaho@gmail.com>
2025-11-02 18:30:35 +0330

    finshing the header and footer in mobile and desktop

--- footer.php
+++ footer.php
@@ -1,91 +1,70 @@
  <!-- Footer -->
  <footer id="site-footer">
-     <div class="container-fluid">
-         <div class="site-footer-container mt-5">
-             <div class="row">
-                 <div class="col-md-5 footer-column">
-                     <h5>خدمات و سرویس‌ها</h5>
-                     <ul>
-                         <li><a href="#">اپ استور</a></li>
-                         <li><a href="#">مک‌استور</a></li>
-                         <li><a href="#">توسعه‌دهندگان</a></li>
-                     </ul>
-
-                     <h5>اپ‌استور</h5>
-                     <ul>
-                         <li><a href="#">آشنایی با اپ‌استور ایرانی سیبانه</a></li>
-                         <li><a href="#">آشنایی با آرکید</a></li>
-                         <li><a href="#">آشنایی با اشتراک‌ها</a></li>
-                         <li><a href="#">دانلود اپلیکیشن سیبانه</a></li>
-                         <li><a href="#">نرم‌افزارهای‌خارجی ویژه</a></li>
-                         <li><a href="#">نرم‌افزارهای‌ایرانی ویژه</a></li>
-                         <li><a href="#">دریافت‌دمو رایگان</a></li>
-                     </ul>
-                 </div>
-
-                 <div class="col-md-5 footer-column">
-                     <h5>همکاری با توسعه‌دهندگان و شرکت‌ها</h5>
-                     <ul>
-                         <li><a href="#">پیشنهادات و ایده‌های همکاری</a></li>
-                         <li><a href="#">انتشار اپ‌در اپ‌استور اپل</a></li>
-                         <li><a href="#">انتشار اپ‌در اپ‌استور سیبانه</a></li>
-                         <li><a href="#">اکانت‌دولوپر اپل</a></li>
-                         <li><a href="#">دریافت‌نشان‌دانلود از سیبانه</a></li>
-                     </ul>
-                     <h5 class="mt-4">آموزش‌ها</h5>
-                     <ul>
-                         <li><a href="#">آموزش نصب سیبانه</a></li>
-                         <li><a href="#">سوالات متداول در مورد سیبانه</a></li>
-                         <li><a href="#">مرتبط‌با اپل</a></li>
-                     </ul>
-                 </div>
-                 <div class="col-md-2 footer-column">
-                     <h5>ارزش‌های سیبانه</h5>
-                     <ul>
-                         <li><a href="#">خط مشی سیبانه</a></li>
-                         <li><a href="#">دریافت بازخورد و بهبود مستمر</a></li>
-                         <li><a href="#">حرکت در چهارچوب قوانین اپل</a></li>
-                         <li><a href="#">چرا سیبانه منحصربفرد است؟</a></li>
-                     </ul>
-                     <h5 class="mt-4">پشتیبانی</h5>
-                     <ul>
-                         <li><a href="#">انجمن پرسش و پاسخ</a></li>
-                         <li><a href="#">ارتباط با کارشناسان</a></li>
-                         <li><a href="#">تماس با ما</a></li>
-                         <li><a href="#">مشاوره رایگان قبل از خرید</a></li>
-                         <li><a href="#">DMCA</a></li>
-                     </ul>
-                 </div>
-
-             </div>
-             <div class="footer-info">
-                 <div>
-                     <p>برای کسب اطلاعات بیشتر در مورد سیبانه بر روی <a href="#">درباره سیبانه</a> کلیک کنید یا
-                         با شماره
-                         ۹۹۹۹۸۸۶۱۰۱ تماس برقرار نمایید</p>
-                 </div>
-                 <div>
-                     <p>info@sibaneh.com</p>
-                 </div>
-             </div>
-             <div class="footer-bottom">
-                 <div>
-                     <span>کپی‌رایت © ۲۰۲۵ سیبانه. تمامی حقوق مادی و معنوی محفوظ میباشد.</span>
-                 </div>
-                 <div>
-                     <a href="#" class="">قوانین</a> | <a href="#" class="">نقشه سایت</a>
-                 </div>
-                 <div class="social-icons">
-                     <a href="#"><i class="icon-twitter"></i></a>
-                     <a href="#"><i class="icon-facebook"></i></a>
-                     <a href="#"><i class="icon-instagram-logo"></i></a>
-                     <a href="#"><i class="icon-tg"></i></a>
-                 </div>
-
-             </div>
-         </div>
-     </div>
- </footer>
+    <div class="container-fluid">
+        <div class="site-footer-container mt-5">
+            <div class="footer-top">
+                <div class="Special-features-icons">
+                    <ul>
+                        <li><a href="#"><i class="icon-box"></i><span>ارسال سریع</span></a></li>
+                        <li><a href="#"><i class="icon-curved-arrow"></i><span>ضمانت مرجوعی</span></a></li>
+                        <li><a href="#"><i class="icon-quality-assurance"></i><span>تضمین کیفیت</span></a></li>
+                        <li><a href="#"><i class="icon-support"></i><span>خدمات پس از فروش</span></a></li>
+                    </ul>
+                </div>
+                <div class="Special-features-email">
+                    <a href=""><span>دریافت ایمیل‌های محصولات جدید</span><i class="icon-up-left-arrow"></i></a>
+                </div>
+            </div>
+            <div class="footer-middle">
+                <div class="footer-column contact-column">
+                    <ul>
+                        <li><a href="#"><i class="icon-support_icon"></i><span>+۹۸۴۱۳۳۳۱۲۳۱۲</span></a></li>
+                        <li><a href="#"><i class="icon-support_icon"></i><span>+۹۸۴۱۳۳۳۱۲۳۱۲</span></a></li>
+                        <li><a href="#"><i class="icon-gps-logo"></i><span>جاده الگولی - فلکه خیام - کوچه سوم - پلاک ۴۳</span></a></li>
+                        <li><a href="#"><i class="icon-envelope"></i><span>info@akamod.com</span></a></li>
+                    </ul>
+                </div>
+                <div class="footer-links-group">
+                    <div class="footer-column links-column">
+                        <ul>
+                            <li><a href="#"><span>تماس با ما</span></a></li>
+                            <li><a href="#"><span>ارسال کالا</span></a></li>
+                            <li><a href="#"><span>ارتباط با ما</span></a></li>
+                            <li><a href="#"><span>ارسال کالا</span></a></li>
+                            <li><a href="#"><span>قوانین و مقررات</span></a></li>
+                            <li><a href="#"><span>قوانین و مقررات</span></a></li>
+                            <li><a href="#"><span>ارسال کالا</span></a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-column links-column">
+                        <ul>
+                            <li><a href="#"><span>تماس با ما</span></a></li>
+                            <li><a href="#"><span>ارسال کالا</span></a></li>
+                            <li><a href="#"><span>ارتباط با ما</span></a></li>
+                            <li><a href="#"><span>ارسال کالا</span></a></li>
+                            <li><a href="#"><span>قوانین و مقررات</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer-column img-column">
+                    <ul>
+                        <li><img class="slide-bg" src="<?php echo get_template_directory_uri(); ?>/images/temp/nomad.png" alt="نماد"></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div>
+                    <span>تمامی حقوق برای آکامد محفوظ است.</span>
+                </div>
+                <div class="social-icons">
+                    <a href="#"><i class="icon-whatsapp"></i></a>
+                    <a href="#"><i class="icon-instagram-aka"></i></a>
+                    <a href="#"><i class="icon-telegram-aka"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
  <?php wp_footer(); ?>
  </body>
 
